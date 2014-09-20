37fea444739bb7f4bf7b249e8bbffdd68f7375d3
Ben Bahrenburg <ben.bahrenburg@gmail.com>
2014-09-20 14:59:42 -0700

    add schedule logic

--- storelist/app/controllers/index.js
+++ storelist/app/controllers/index.js
@@ -31,27 +31,34 @@ var storeManager = {
 	    store.save();
 	    stores.fetch();		
 	},
-	networkNotAvailable : function(){
+	scheduledRefresh : function(){
+		var refreshedOn = new Date(parseFloat(Ti.App.Properties.getDouble('LAST_REFESHED',0)));
+		var moreThanAWeek = ((new Date() - refreshedOn)/(1000*60*60*24) > 7);
+		
+		if(Ti.Network.online && moreThanAWeek){
+			storeManager.refreshAll();
+			Ti.App.Properties.setDouble('LAST_REFESHED', new Date().getTime());
+		}
+		
+	},
+	refreshAll : function(){
 		if(!Ti.Network.online){
 			alert('A network connection is needed to refresh');
 			return true;
-		}		
-		return false;
-	},
-	refreshAll : function(){
-		if(remoteRefresh.networkNotAvailable()){
-			return;
-		}
-		stores.removeAll(); //Remove all local records
+		}	
 		//Call to MongoLab to get the full Store Collection
 		mongoClient.getDocuments('store',function(data){
 			if(!data.sucess){
 				alert("Error: " + JSON.stringify(data.message));
 				return;
 			}
-			_.each(data.docs, remoteRefresh.addStore);
+			stores.removeAll(); //Remove all local records
+			//Loop through each of the documents returned and save locally
+			_.each(data.docs, storeManager.addStore);
 		});
 	}
 };
+
+$.index.addEventListener('focus',storeManager.scheduledRefresh);
 
 $.index.open();
