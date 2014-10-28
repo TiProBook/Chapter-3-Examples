6b519bc2364c0aa273e4bf67977c74cd6685dd7a
Ben Bahrenburg <ben.bahrenburg@gmail.com>
2014-10-28 15:11:22 -0700

    add mongo writer example

--- MongoWriter/app/controllers/index.js
+++ MongoWriter/app/controllers/index.js
@@ -1,5 +1,39 @@
-function doClick(e) {
-    alert($.label.text);
-}
+var mongo = require('ti.mongolab.client');
+
+var mongoClient = new mongo({
+	debug : true,
+	timeout : 15000,
+	apiKey: "your mongo lab apikey goes here",
+	dbName :'event-log' //"your database name goes here"	
+});
+
+function addLogEntry(){
+	var logEntry = {
+		message:'hello word ' + new Date().getTime(),
+		level : 'informational',
+		timestamp : new Date().getTime()
+	};
+	
+	mongoClient.createDocument('eventLog',logEntry,function(data){
+		var message = (data.sucess) ? 'Event Log Added' : 'Error:' + JSON.stringify(data.message);
+		alert(message);
+	});	
+};
+
+function removeAllLogs(){
+	var startingDate = new Date();
+	startingDate.setDate(startingDate.getDate()-1);	
+	
+	var query = {
+		q: {
+			timestamp: { $gt: startingDate.getTime() }
+		}
+	};
+	
+	mongoClient.deleteDocuments('eventLog',query,function(data){
+		var message = (data.sucess) ? 'Event Log Cleared' : 'Error:' + JSON.stringify(data.message);
+		alert(message);
+	});	
+};
 
 $.index.open();
